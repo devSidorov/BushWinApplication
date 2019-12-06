b10aea01b905d7792179575b22cff8f46a784473
Юрий Сидоров <ysidorov@ogk.delta>
2019-12-06 18:12:26 +0300

    Добавлен наследуемый класс для очереди с лимитом по местам
    +конструктор, добавление элемента с проверкой

--- BushWinApplication/Include/QueueTemplate.cpp
+++ BushWinApplication/Include/QueueTemplate.cpp
@@ -1,17 +1,26 @@
 #include "QueueTemplate.h"
 
 template<class T>
+NodeTemplate<T>::NodeTemplate()
+{
+	memset( *m_data, NULL, sizeof( T ) );
+	m_pNextLink = nullptr;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
 QueueLinkedList<T>::QueueLinkedList()
 {
-	m_pFirstNode = m_pCurrentNode = nullptr;
+	m_pFirstNode = m_pLastNode = nullptr;
 }
 
 template<class T>
-QueueLinkedList<T>::QueueLinkedList( T data )
+QueueLinkedList<T>::QueueLinkedList( T& data )
 {
-	m_pFirstNode = m_pCurrentNode = new NodeTemplate;
-	m_pCurrentNode->m_data = data;
-	m_pCurrentNode->m_pNextLink = nullptr;
+	m_pFirstNode = m_pLastNode = new NodeTemplate;
+	m_pLastNode->m_data = data;
+	m_pLastNode->m_pNextLink = nullptr;
 }
 
 template<class T>
@@ -19,10 +28,10 @@ QueueLinkedList<T>::~QueueLinkedList()
 {
 	NodeTemplate* pTempNode = nullptr;
 	
-	while ( m_pCurrentNode ) //when it comes to nullptr its last link, memory clean
+	while ( m_pLastNode ) //when it comes to nullptr its last link, memory clean
 	{
-		pTempNode = m_pCurrentNode;
-		m_pCurrentNode = m_pCurrentNode->m_pNextLink;
+		pTempNode = m_pLastNode;
+		m_pLastNode = m_pLastNode->m_pNextLink;
 		delete pTempNode;
 	}
 }
@@ -30,25 +39,69 @@ QueueLinkedList<T>::~QueueLinkedList()
 template<class T>
 void QueueLinkedList<T>::fnAddEmptyNode()
 {
-	m_pCurrentNode->m_pNextLink = new NodeTemplate;
-	m_pCurrentNode = m_pCurrentNode->m_pNextLink;
+	if ( fnIsEmpty )
+	{
+		m_pLastNode = m_pFirstNode = new NodeTemplate;
+	}
+	else
+	{
+		m_pLastNode->m_pNextLink = new NodeTemplate;
+		m_pLastNode = m_pLastNode->m_pNextLink;
+	}
 }
 
 template<class T>
-void QueueLinkedList<T>::fnAddNode( T data )
+bool QueueLinkedList<T>::fnIsEmpty()
+{
+	return ( m_pFirstNode == nullptr );
+}
+
+template<class T>
+void QueueLinkedList<T>::fnPush( T& data )
 {
 	fnAddEmptyNode();
-	m_pCurrentNode->m_data = data;
+	m_pLastNode->m_data = data;
 }
 
 template<class T>
-void QueueLinkedList<T>::fnGetDataInQueue( T& data )
+void QueueLinkedList<T>::fnPop( T& data )
 {
 	NodeTemplate* pProcessedNode = m_pFirstNode;
 	
 	data = m_pFirstNode->m_data;
 
 	m_pFirstNode = m_pFirstNode->m_pNextLink;
+	if ( fnIsEmpty )
+		m_pLastNode = nullptr;
 	delete pProcessedNode;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template<class T>
+bool LimitedQueue<T>::fnIsEmpty()
+{
+	return QueueLinkedList< T >::fnIsEmpty();
+}
+
+template<class T>
+void LimitedQueue<T>::fnPush( T & data )
+{
+	if ( m_limitPos )
+		while ( m_queueSize >= m_limilPos )
+		{
+			T tempData;
+			QuueLinkedList< T >::fnPop( tempData );
+			--m_queueSize;
+		}
+
+	QuueLinkedList< T >::fnPush( data );
+	++m_queueSize;
+}
+
+template<class T>
+void LimitedQueue<T>::fnPop( T & data )
+{
+}
+
 
--- BushWinApplication/Include/QueueTemplate.h
+++ BushWinApplication/Include/QueueTemplate.h
@@ -1,29 +1,50 @@
 #pragma once
+#include <memory.h>
 
 template <class T>
 struct NodeTemplate
 {
 	T m_data;
 	NodeTemplate* m_pNextLink;
+
+	NodeTemplate();
 };
 
 template <class T>
 class QueueLinkedList
 {
 private:
-	NodeTemplate* m_pCurrentNode;
+	NodeTemplate* m_pLastNode;
 	NodeTemplate* m_pFirstNode;
-	
+
+private:
+	void fnAddEmptyNode();
 
 public:
 	QueueLinkedList();
-	QueueLinkedList( T data );
+	QueueLinkedList( T& data );
 	~QueueLinkedList();
 
-	void fnAddEmptyNode();
-	void fnAddNode( T data );
+	virtual bool fnIsEmpty();
+	virtual void fnPush( T& data );
+	virtual void fnPop( T& data );
+};
 
-	void fnGetDataInQueue( T& data );
+template <class T>
+class LimitedQueue : private QueueLinkedList< T >
+{
+private:
+	unsigned int m_LimitPos;
+	unsigned int m_QueueSize;
+
+public:
+	LimitedQueue( unsigned int limit = NULL ) : m_limitPos( limit ), m_QueueSize( NULL ) {};
+	LimitedQueue( T& data, unsigned int limit = NULL ) : QueueLinkedList( data ), 
+		m_limitPos( limit ), m_QueueSize( 1 ) {};
+
+	virtual bool fnIsEmpty();
+	virtual void fnPush( T& data );
+	virtual void fnPop( T& data );
 
 };
 
